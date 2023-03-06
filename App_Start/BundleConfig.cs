dd47b6d46e559b30ca07edd3cc35ad0e4a53541b
CristiDS-Work <126245567+CristiDS-Work@users.noreply.github.com>
2023-03-06 08:11:06 +0200

    Add files via upload

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+
+namespace SitecorePoweredBlog
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css/bootstrap").Include(
+                        "~/Content/css/bootstrap-themed.min.css")
+                        );
+
+            bundles.Add(new StyleBundle("~/Content/css/custom").Include(
+                        "~/Content/css/blogsite.css")
+                        );
+
+            bundles.Add(new ScriptBundle("~/Scripts/default").Include(
+                        "~/Scripts/modernizr-2.8.3.js")
+                        );
+
+            bundles.Add(new ScriptBundle("~/Scripts/bootstrap").Include(
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/bootstrap.min.js")
+                        );
+
+            bundles.Add(new ScriptBundle("~/Scripts/jquery").Include(
+                        "~/Scripts/jquery-3.3.1.slim.min.js")
+                        );
+        }
+    }
+}
---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SitecorePoweredBlog
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Blog",
+                url: "Blog/{id}",
+                defaults: new { controller = "Blog", action = "Index", id = 1 },
+                constraints: new { id = @"\d+" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
